ab6ead530dfb705e98a7e579843c84c8a5d01812
Yael <yael.levyy@gmail.com>
2021-01-12 21:04:38 +0200

    carusela files

---
+++ main.js
@@ -0,0 +1,19 @@
+var slider = tns({
+  container: ".my-slider",
+  autoplay: true,
+  speed: 10000,
+  items: 1,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2,
+    },
+    700: {
+      gutter: 30,
+    },
+    900: {
+      items: 3,
+    },
+  },
+});
